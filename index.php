82a0a26a8da9f24950bb1eb944a53e4723c24d9a
reemss22 <75126174+reemss22@users.noreply.github.com>
2020-11-28 03:32:45 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>صفحتي الاوليtitle>
+</head>
+<body bgcolor="red">
+<?php
+
+
+
+echo "بسم الله الله الرحمن الرحيم"
+echo "كليه الامارات للعلوم والتكنولوجيا"
+   echo "بكالاريوس تقانه المعلومات"
+   echo "reem galal"
+   echo "السكن : الكلاكله"
+   echo ""العمر :20
+   for($x=1;$x<=10;$x++)
+	   echo $x."<br>";
+   
+    ?>
+</body>
+</html>
